0477ce624aded771c057189812785bad6fe70ba9
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2026-03-30 11:18:54 +0200

    add custom theme

--- wp-content/plugins/arabic-academy/arabic-academy.php
+++ wp-content/plugins/arabic-academy/arabic-academy.php
@@ -16,15 +16,15 @@ if(!function_exists('arabic_academy_register_custom_post_types_and_taxonomies'))
     function arabic_academy_register_custom_post_types_and_taxonomies(){
         // Lesson Post Type
         $labels = [
-            'name'          => __( 'Lessons', 'arabic_academy' ),
-			'singular_name' => __( 'Lesson', 'arabic_academy' ),
-            'search_items'      => __( 'Search Lessons', 'arabic_academy' ),
-            'all_items'         => __( 'All Lessons', 'arabic_academy' ),
-            'edit_item'         => __( 'Edit Lesson', 'arabic_academy' ),
-            'update_item'       => __( 'Update Lesson', 'arabic_academy' ),
-            'add_new_item'      => __( 'Add New Lesson', 'arabic_academy' ),
-            'new_item_name'     => __( 'New Lesson Name', 'arabic_academy' ),
-            'menu_name'         => __( 'Lessons', 'arabic_academy' ),
+            'name'          => __( 'Lessons', 'arabic-academy' ),
+			'singular_name' => __( 'Lesson', 'arabic-academy' ),
+            'search_items'      => __( 'Search Lessons', 'arabic-academy' ),
+            'all_items'         => __( 'All Lessons', 'arabic-academy' ),
+            'edit_item'         => __( 'Edit Lesson', 'arabic-academy' ),
+            'update_item'       => __( 'Update Lesson', 'arabic-academy' ),
+            'add_new_item'      => __( 'Add New Lesson', 'arabic-academy' ),
+            'new_item_name'     => __( 'New Lesson Name', 'arabic-academy' ),
+            'menu_name'         => __( 'Lessons', 'arabic-academy' ),
         ];
         $args = [
             'labels'      => $labels,
@@ -37,15 +37,15 @@ if(!function_exists('arabic_academy_register_custom_post_types_and_taxonomies'))
 
         // Test Post Type
         $labels = [
-            'name'          => __( 'Tests', 'arabic_academy' ),
-            'singular_name' => __( 'Test', 'arabic_academy' ),
-            'search_items'      => __( 'Search Tests', 'arabic_academy' ),
-            'all_items'         => __( 'All Tests', 'arabic_academy' ),
-            'edit_item'         => __( 'Edit Test', 'arabic_academy' ),
-            'update_item'       => __( 'Update Test', 'arabic_academy' ),
-            'add_new_item'      => __( 'Add New Test', 'arabic_academy' ),
-            'new_item_name'     => __( 'New Test Name', 'arabic_academy' ),
-            'menu_name'         => __( 'Tests', 'arabic_academy' ),
+            'name'          => __( 'Tests', 'arabic-academy' ),
+            'singular_name' => __( 'Test', 'arabic-academy' ),
+            'search_items'      => __( 'Search Tests', 'arabic-academy' ),
+            'all_items'         => __( 'All Tests', 'arabic-academy' ),
+            'edit_item'         => __( 'Edit Test', 'arabic-academy' ),
+            'update_item'       => __( 'Update Test', 'arabic-academy' ),
+            'add_new_item'      => __( 'Add New Test', 'arabic-academy' ),
+            'new_item_name'     => __( 'New Test Name', 'arabic-academy' ),
+            'menu_name'         => __( 'Tests', 'arabic-academy' ),
         ];
         $args = [
             'labels'      => $labels,
@@ -58,15 +58,15 @@ if(!function_exists('arabic_academy_register_custom_post_types_and_taxonomies'))
 
         // Class Taxonomy
         $labels = array(
-            'name'              => _x( 'Classes', 'taxonomy general name', 'arabic_academy' ),
-            'singular_name'     => _x( 'Class', 'taxonomy singular name', 'arabic_academy' ),
-            'search_items'      => __( 'Search Classes', 'arabic_academy' ),
-            'all_items'         => __( 'All Classes', 'arabic_academy' ),
-            'edit_item'         => __( 'Edit Class', 'arabic_academy' ),
-            'update_item'       => __( 'Update Class', 'arabic_academy' ),
-            'add_new_item'      => __( 'Add New Class', 'arabic_academy' ),
-            'new_item_name'     => __( 'New Class Name', 'arabic_academy' ),
-            'menu_name'         => __( 'Classes', 'arabic_academy' ),
+            'name'              => _x( 'Classes', 'taxonomy general name', 'arabic-academy' ),
+            'singular_name'     => _x( 'Class', 'taxonomy singular name', 'arabic-academy' ),
+            'search_items'      => __( 'Search Classes', 'arabic-academy' ),
+            'all_items'         => __( 'All Classes', 'arabic-academy' ),
+            'edit_item'         => __( 'Edit Class', 'arabic-academy' ),
+            'update_item'       => __( 'Update Class', 'arabic-academy' ),
+            'add_new_item'      => __( 'Add New Class', 'arabic-academy' ),
+            'new_item_name'     => __( 'New Class Name', 'arabic-academy' ),
+            'menu_name'         => __( 'Classes', 'arabic-academy' ),
         );
         $args   = array(
             'labels'            => $labels,
@@ -80,15 +80,15 @@ if(!function_exists('arabic_academy_register_custom_post_types_and_taxonomies'))
 
         // Branch Taxonomy
         $labels = array(
-            'name'              => _x( 'Branches', 'taxonomy general name', 'arabic_academy' ),
-            'singular_name'     => _x( 'Branch', 'taxonomy singular name', 'arabic_academy' ),
-            'search_items'      => __( 'Search Branches', 'arabic_academy' ),
-            'all_items'         => __( 'All Branches', 'arabic_academy' ),
-            'edit_item'         => __( 'Edit Branch', 'arabic_academy' ),
-            'update_item'       => __( 'Update Branch', 'arabic_academy' ),
-            'add_new_item'      => __( 'Add New Branch', 'arabic_academy' ),
-            'new_item_name'     => __( 'New Branch Name', 'arabic_academy' ),
-            'menu_name'         => __( 'Branches', 'arabic_academy' ),
+            'name'              => _x( 'Branches', 'taxonomy general name', 'arabic-academy' ),
+            'singular_name'     => _x( 'Branch', 'taxonomy singular name', 'arabic-academy' ),
+            'search_items'      => __( 'Search Branches', 'arabic-academy' ),
+            'all_items'         => __( 'All Branches', 'arabic-academy' ),
+            'edit_item'         => __( 'Edit Branch', 'arabic-academy' ),
+            'update_item'       => __( 'Update Branch', 'arabic-academy' ),
+            'add_new_item'      => __( 'Add New Branch', 'arabic-academy' ),
+            'new_item_name'     => __( 'New Branch Name', 'arabic-academy' ),
+            'menu_name'         => __( 'Branches', 'arabic-academy' ),
         );
         $args   = array(
             'labels'            => $labels,
