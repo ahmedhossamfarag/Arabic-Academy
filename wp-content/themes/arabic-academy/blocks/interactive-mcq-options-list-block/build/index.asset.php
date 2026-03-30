d31332f0c3204f3574c93844845dd6be77d7c7a5
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2026-03-30 18:11:31 +0200

    fix: interactive-mcq-options-list-block style

--- wp-content/themes/arabic-academy/blocks/interactive-mcq-options-list-block/build/index.asset.php
+++ wp-content/themes/arabic-academy/blocks/interactive-mcq-options-list-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e8a70bce43eca7973e2d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '4a635581832ba7913260');
