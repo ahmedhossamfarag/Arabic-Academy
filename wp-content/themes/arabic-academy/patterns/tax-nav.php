906f64d9006d6fda9126e6f3be584dc4efb40083
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2026-03-31 16:25:38 +0200

    update lesson & test patterns

--- wp-content/themes/arabic-academy/patterns/tax-nav.php
+++ wp-content/themes/arabic-academy/patterns/tax-nav.php
@@ -5,13 +5,11 @@
  * Categories: 
  */
 ?>
-<!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-3-f-4949"}}},"typography":{"fontSize":"16px"}},"textColor":"custom-3-f-4949"} -->
-<p class="has-custom-3-f-4949-color has-text-color has-link-color" style="font-size:16px"><?php esc_html_e('المرحلة الثانوية', 'arabic-academy');?></p>
-<!-- /wp:paragraph -->
+<!-- wp:group {"templateLock":false,"lock":{"move":false,"remove":false},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:post-terms {"term":"class","style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-3-f-4949"}}},"typography":{"fontSize":"16px"}},"textColor":"custom-3-f-4949"} /-->
 
 <!-- wp:generateblocks/shape {"uniqueId":"2652f11d","styles":{"display":"inline-flex","svg":{"fill":"currentColor","height":"16px","width":"16px","color":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dcustom-3-f-4949, #3f4949)"}},"css":".gb-shape-2652f11d{display:inline-flex}.gb-shape-2652f11d svg{fill:currentColor;height:16px;width:16px;color:var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dcustom-3-f-4949,#3f4949)}"} -->
-<span class="gb-shape gb-shape-2652f11d"><svg aria-hidden="true" height="1em" width="1em" viewBox="0 0 256 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg></span>
+<span class="gb-shape gb-shape-2652f11d"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" width="1em" height="1em" aria-hidden="true"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" fill="currentColor"></path></svg></span>
 <!-- /wp:generateblocks/shape -->
 
 <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-00464-a"}}},"typography":{"fontSize":"16px"}},"textColor":"custom-00464-a"} -->
--- wp-content/themes/arabic-academy/patterns/test-content.php
+++ wp-content/themes/arabic-academy/patterns/test-content.php
@@ -8,13 +8,11 @@
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"96px","bottom":"96px"}}},"backgroundColor":"custom-f-8-fafb","layout":{"type":"constrained","contentSize":"1200px"}} -->
-<div class="wp-block-group has-custom-f-8-fafb-background-color has-background" style="padding-top:96px;padding-bottom:96px"><!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"metadata":{"patternName":"core/block/141","name":"Tax Nav"},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-3-f-4949"}}},"typography":{"fontSize":"16px"}},"textColor":"custom-3-f-4949"} -->
-<p class="has-custom-3-f-4949-color has-text-color has-link-color" style="font-size:16px"><?php esc_html_e('المرحلة الثانوية', 'arabic-academy');?></p>
-<!-- /wp:paragraph -->
+<div class="wp-block-group has-custom-f-8-fafb-background-color has-background" style="padding-top:96px;padding-bottom:96px"><!-- wp:group {"templateLock":false,"lock":{"move":false,"remove":false},"metadata":{"categories":[],"patternName":"core/block/263","name":"Tax Nav"},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:post-terms {"term":"class","style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-3-f-4949"}}},"typography":{"fontSize":"16px"}},"textColor":"custom-3-f-4949"} /-->
 
-<!-- wp:generateblocks/shape {"uniqueId":"2652f11d","styles":{"display":"inline-flex","svg":{"fill":"currentColor","height":"16px","width":"16px","color":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dcustom-3-f-4949, #3f4949)"}},"css":".gb-shape-2652f11d{display:inline-flex}.gb-shape-2652f11d svg{fill:currentColor;height:16px;width:16px;color:var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dcustom-3-f-4949,#3f4949)}"} -->
-<span class="gb-shape gb-shape-2652f11d"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" width="1em" height="1em" aria-hidden="true"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" fill="currentColor"></path></svg></span>
+<!-- wp:generateblocks/shape {"uniqueId":"ab5bcf52","styles":{"display":"inline-flex","svg":{"fill":"currentColor","height":"16px","width":"16px","color":"var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dcustom-3-f-4949, #3f4949)"}},"css":".gb-shape-ab5bcf52{display:inline-flex}.gb-shape-ab5bcf52 svg{fill:currentColor;height:16px;width:16px;color:var(\u002d\u002dwp\u002d\u002dpreset\u002d\u002dcolor\u002d\u002dcustom-3-f-4949,#3f4949)}"} -->
+<span class="gb-shape gb-shape-ab5bcf52"><svg aria-hidden="true" height="1em" width="1em" viewBox="0 0 256 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg></span>
 <!-- /wp:generateblocks/shape -->
 
 <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|custom-00464-a"}}},"typography":{"fontSize":"16px"}},"textColor":"custom-00464-a"} -->
